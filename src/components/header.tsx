6a3d705d345bbe49212de31bae1696071f67f18e
Daniel Chrisostomo <daniluz1515@gmail.com>
2025-01-24 11:45:41 -0300

    Módulo: Páginas e Componentes (UI) completo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
+import { AccountMenu } from './account-menu'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -19,6 +21,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Dashboard } from './pages/app/dashboard';
+import { Dashboard } from './pages/app/dashboard/dashboard';
 import { AppLayout } from './pages/_layouts/app';
 import { AuthLayout } from './pages/_layouts/auth';
 import { SignIn } from './pages/auth/sign-in';
 import { SignUp } from './pages/auth/sign-up';
+import { Orders } from './pages/app/orders/orders';
+import { NotFound } from './pages/404';
 
 export const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
